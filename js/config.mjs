ccd6c47986a6b8882ed4aed40074f5dce34143d9
Tim van der Lippe <Tim.Lippe@logius.nl>
2025-08-20 15:07:56 +0200

    Beheer: gebruik nieuwe organisation-config setup
    Part of Logius-standaarden/Automatisering#51

--- js/config.mjs
+++ js/config.mjs
@@ -1,4 +1,6 @@
-var respecConfig = {
+import { loadRespecWithConfiguration } from "https://logius-standaarden.github.io/publicatie/respec/organisation-config.mjs";
+
+loadRespecWithConfiguration({
   alternateFormats: [ { 
         "label" : "pdf",
         "uri" : "BOMOS-Beheermodel.pdf"
@@ -30,4 +32,4 @@ var respecConfig = {
   shortName: "beheer",
   specStatus: "WV",
   specType: "HR"
-};
+});
